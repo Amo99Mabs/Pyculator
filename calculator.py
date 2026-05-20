2a1fbd36787501ce76dcb84af87f6545f2393df0
Amogelang (Amo) Mabonela <mabonelaa@gmail.com>
2026-05-20 21:46:10 +0200

    Update calculator.py.

--- calculator.py
+++ calculator.py
@@ -12,7 +12,7 @@ elif operator == "*":
     result = num1 * num2
     print(round(result, 3))
 elif operator == "/":
-    result = num1 / num2
+    result = num1 / num2 
     print(round(result, 3))
 else:
     print(f"{operator} is not a valid operator")
